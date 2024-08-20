ac451565bf359fa476d4ad352998ebf93ba70843
rcodyp <rahul887393@gmail.com>
2024-08-20 08:31:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express =  require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+app.set("view engine", "ejs"); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, "public"))); 
+
+app.get('/', function(req, res){
+    fs.readdir(`./files`, function(err, files){
+        console.log(files);
+        res.render("index", {files: files});
+    })
+});
+
+app.post('/create', function(req, res){
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
+        res.redirect("/");
+    });
+    console.log(req.body);
+});
+
+app.listen(3000);         
